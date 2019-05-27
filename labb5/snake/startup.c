62ca8915727ddbb57c1b46516548590c08719c6d
Love Lyckaro <love.lyckaro@hotmail.se>
2019-05-27 09:02:14 +0200

    Add empty snake project

---
+++ labb5/snake/startup.c
@@ -0,0 +1,24 @@
+/*
+ * 	startup.c
+ *  
+ */
+#include "libGPIO.h"
+#include <stdint.h>
+
+void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
+
+void startup ( void )
+{
+asm volatile(
+	" LDR R0,=0x2001C000\n"		/* set stack */
+	" MOV SP,R0\n"
+	" BL main\n"				/* call main */
+	".L1: B .L1\n"				/* never return */
+	) ;
+}
+
+int main(void)
+{
+    return 0;
+}
+
